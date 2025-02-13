af99e51194103e5556c1b9b2188e121ffe95f53a
Sala N 503 <salan503@salasfr.itm.edu.co>
2025-02-12 21:35:17 -0500

    listas y tuplas

---
+++ ejercicioUno.py
@@ -0,0 +1,57 @@
+import keyword
+
+
+# print(keyword.kwlist)
+# print(len(keyword.kwlist))
+
+
+# a = 5 
+
+# print(type(a))
+
+# b = 5. 
+
+# print(type(b))
+
+# c = 1 + 2j
+
+# print(type(a))
+
+# f = True
+
+# print(type(f))
+
+
+# LISTAS Y TUPLAS 
+
+
+lista = [1,2,3,4,5,6,7,8]
+
+tupla = 1,2,3,4,5,6
+
+listaDos = [1, 3.1416, 1+2j, "hola mundo", False]
+
+
+listaDos[2] = 3.14159
+
+
+listaTres = [3.14, 2, [1,2,3]]
+listaTres = [3.14, 2, True]
+
+
+listaCuatro= [3.14, 2,[1,[(1+2j, (0.5 + 0.5j), 3j)], 3]]
+
+
+
+
+# tuplaDos = 
+
+# print(listaTres[-1])
+print(listaCuatro)
+
+# print(tupla)
+
+ # print(type(lista))
+# print (lista[2: 6])
+# print (lista[: 6])
+# print (tupla[-1])
